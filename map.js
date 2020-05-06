1f2ddc580c429978d5daacbd36cfe14354e47701
dangolsayara <sayaradangol11@gmail.com>
2020-05-06 20:10:32 +0545

    first commit

---
+++ map.js
@@ -0,0 +1,5 @@
+var arr=[10,20,30,40,50];
+var newArr= arr.map(function(val){  // map creates a new array from prev array
+    console.log(val);
+});
+console.log(arr);// previous array
---
+++ map1.js
@@ -0,0 +1,14 @@
+var arr=[
+    {name:"rohina", age:"30",gender:"f"},
+    {name:"pravash", age:"25",gender:"m"},
+    {name:"osina", age:"20",gender:"f"},
+    {name:"rohit", age:"20",gender:"m"},
+    {name:"hina", age:"26",gender:"f"}
+
+];
+var newArr = arr.map(value => value.name);
+console.log(newArr);
+
+
+
+
